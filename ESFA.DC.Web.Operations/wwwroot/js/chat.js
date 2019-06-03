b7484929316ede3bde9600213943017fa3ecff72
jamie <jamie.coales@education.gov.uk>
2019-06-03 13:58:14 +0100

    logging

--- ESFA.DC.Web.Operations/wwwroot/js/chat.js
+++ ESFA.DC.Web.Operations/wwwroot/js/chat.js
@@ -1,6 +1,7 @@
 ﻿"use strict";
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+connection.hub.logging = true;
 
 //Disable send button until connection is established
 document.getElementById("sendButton").disabled = true;
