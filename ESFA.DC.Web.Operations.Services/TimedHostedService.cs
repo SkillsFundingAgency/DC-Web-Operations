86859bae52302c7e572186edf0d1c7bcb945751e
John Youren <john.youren@gmail.com>
2019-06-24 16:00:59 +0100

    Added timer logic to timer hosted service

--- ESFA.DC.Web.Operations.Services/TimedHostedService.cs
+++ ESFA.DC.Web.Operations.Services/TimedHostedService.cs
@@ -12,8 +12,14 @@ namespace ESFA.DC.Web.Operations.Services
     {
         private readonly ILogger _logger;
         private readonly IPeriodEndService _periodEndService;
-        private Timer _timer;
         private readonly PeriodEndHub _periodEndHub;
+
+        private readonly ManualResetEvent timerResetEvent = new ManualResetEvent(false);
+        private readonly object timerStopLock = new object();
+        private readonly object timerChangeLock = new object();
+
+        private Timer _timer;
+        private bool timerStopFlag;
 
         public TimedHostedService(
             ILogger logger,
@@ -27,32 +33,75 @@ namespace ESFA.DC.Web.Operations.Services
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            _logger.LogInfo("Timed Background Service is starting.");
+            _logger.LogInfo("Timed SignalR Background Service is starting.");
 
-            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+            StartTimer();
 
             return Task.CompletedTask;
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            _logger.LogInfo("Timed Background Service is stopping.");
+            _logger.LogInfo("Timed SignalR Background Service is stopping.");
 
-            _timer?.Change(Timeout.Infinite, 0);
+            StopTimer();
 
             return Task.CompletedTask;
         }
 
         public void Dispose()
         {
-            _timer?.Dispose();
+            StopTimer();
         }
 
-        private void DoWork(object state)
+        private void StartTimer()
         {
-            var result = _periodEndService.GetPathItemStates().Result;
+            lock (timerChangeLock)
+            {
+                timerStopFlag = false;
+                timerResetEvent.Set();
+                _timer = new Timer(DoWork, null, TimeSpan.FromSeconds(5).Milliseconds, Timeout.Infinite);
+            }
+        }
 
-            _periodEndHub.SendMessage(result);
+        private void StopTimer()
+        {
+            lock (timerChangeLock)
+            {
+                timerResetEvent.Reset();
+                lock (timerStopLock)
+                {
+                    timerStopFlag = true;
+                }
+
+                if (timerResetEvent.WaitOne(5500))
+                {
+                    _timer?.Dispose();
+                    _timer = null;
+                }
+            }
+        }
+
+        private async void DoWork(object state)
+        {
+            // Get state JSON.
+            string result = await _periodEndService.GetPathItemStates();
+
+            // Send JSON to clients.
+            await _periodEndHub.SendMessage(result);
+
+            // If we're told to stop then set block to continue, and exit method.
+            lock (timerStopLock)
+            {
+                if (timerStopFlag)
+                {
+                    timerResetEvent.Set();
+                    return;
+                }
+            }
+
+            // Set timer to tick in 5 seconds.
+            _timer.Change(TimeSpan.FromSeconds(5).Milliseconds, Timeout.Infinite);
         }
     }
 }
