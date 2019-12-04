65df4cb01428750e8bac85735b05bc62de422dfb
Lewis Malin <lewismalin@hotmail.com>
2019-12-04 11:07:02 +0000

    moved index so it displays

--- ESFA.DC.Web.Operations/Areas/Frm/Controllers/FrmController.cs
+++ ESFA.DC.Web.Operations/Areas/Frm/Controllers/FrmController.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ESFA.DC.Web.Operations.Utils;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ESFA.DC.Web.Operations.Areas.Frm.Controllers
 {
+    [Area(AreaNames.Frm)]
     public class FrmController : Controller
     {
         public IActionResult Index()
