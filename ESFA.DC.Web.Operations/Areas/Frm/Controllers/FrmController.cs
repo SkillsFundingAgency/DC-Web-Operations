11a627de1186df03d030080e94fdab4ee122aaf5
Lewis Malin <lewismalin@hotmail.com>
2019-12-04 10:35:38 +0000

    Added FrmController

---
+++ ESFA.DC.Web.Operations/Areas/Frm/Controllers/FrmController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ESFA.DC.Web.Operations.Areas.Frm.Controllers
+{
+    public class FrmController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
