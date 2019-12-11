db3fbe6fc58b2b8db4eb51d95257fd30bc30760a
Lewis Malin <lewismalin@hotmail.com>
2019-12-11 08:08:06 +0000

    Updated model to allow for error message generation, added blank holding page.

--- ESFA.DC.Web.Operations/Areas/Frm/Models/FrmReportModel.cs
+++ ESFA.DC.Web.Operations/Areas/Frm/Models/FrmReportModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,8 +10,11 @@ namespace ESFA.DC.Web.Operations.Areas.Frm.Models
     {
         public bool IsFrmReportChoice { get; set; }
 
+        [StringLength(4)]
+        [Required(ErrorMessage = "A start date is required")]
         public string FrmYearPeriod { get; set; }
 
+        [Required(ErrorMessage = "A start date is required")]
         public DateTime FrmDate { get; set; }
     }
 }
