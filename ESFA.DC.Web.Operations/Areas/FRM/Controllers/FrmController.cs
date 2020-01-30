024440d2e30c10f677d50c8bc2b75a48dc13a31a
Pete Chamley <peter@chamley.name>
2020-01-30 13:47:04 +0000

    Fix Constranst errors

--- ESFA.DC.Web.Operations/Areas/FRM/Controllers/FrmController.cs
+++ ESFA.DC.Web.Operations/Areas/FRM/Controllers/FrmController.cs
@@ -1,21 +1,23 @@
-﻿namespace ESFA.DC.Web.Operations.Areas.Frm.Controllers
-{
-    using System;
-    using System.Linq;
-    using System.Threading;
-    using System.Threading.Tasks;
-    using ESFA.DC.FileService.Interface;
-    using ESFA.DC.Jobs.Model.Enums;
-    using ESFA.DC.Logging.Interfaces;
-    using ESFA.DC.Web.Operations.Areas.Frm.Models;
-    using ESFA.DC.Web.Operations.Interfaces.Frm;
-    using ESFA.DC.Web.Operations.Interfaces.PeriodEnd;
-    using ESFA.DC.Web.Operations.Interfaces.Storage;
-    using ESFA.DC.Web.Operations.Utils;
-    using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ESFA.DC.FileService.Interface;
+using ESFA.DC.Jobs.Model.Enums;
+using ESFA.DC.Logging.Interfaces;
+using ESFA.DC.Web.Operations.Areas.Frm.Models;
+using ESFA.DC.Web.Operations.Controllers;
+using ESFA.DC.Web.Operations.Interfaces.Frm;
+using ESFA.DC.Web.Operations.Interfaces.PeriodEnd;
+using ESFA.DC.Web.Operations.Interfaces.Storage;
+using ESFA.DC.Web.Operations.Utils;
+using Microsoft.ApplicationInsights;
+using Microsoft.AspNetCore.Mvc;
 
+namespace ESFA.DC.Web.Operations.Areas.Frm.Controllers
+{
     [Area(AreaNames.Frm)]
-    public class FrmController : Controller
+    public class FrmController : BaseControllerWithOpsPolicy
     {
         private readonly IFrmService _frmService;
         private readonly ILogger _logger;
@@ -28,7 +30,9 @@
             IFrmService frmService,
             IPeriodService periodService,
             IStorageService storageService,
-            IFileService fileService)
+            IFileService fileService,
+            TelemetryClient telemetryClient)
+            : base(logger, telemetryClient)
         {
             _logger = logger;
             _frmService = frmService;
@@ -67,7 +71,7 @@
                     var currentPeriod = await _periodService.ReturnPeriod();
                     model.FrmPeriod = $"R{currentPeriod.Period.ToString("D2")}";
                     var collectionYear = currentPeriod.Year.Value;
-                    var currentContainerName = string.Format(Constants.FrmContainerName, collectionYear);
+                    var currentContainerName = string.Format(Utils.Constants.FrmContainerName, collectionYear);
                     var fileMetaData = await _fileService.GetFileMetaDataAsync(currentContainerName, $"FrmFailedFiles_{model.FrmPeriod}.csv", true, CancellationToken.None);
                     model.FrmCSVValidDate = fileMetaData.First().LastModified;
                     return View("ValidateSuccess", model);
@@ -91,7 +95,7 @@
                 throw new Exception(errorMessage);
             }
 
-            model.FrmJobType = Constants.FrmValidationKey;
+            model.FrmJobType = Utils.Constants.FrmValidationKey;
             var frmContainerName = $"frm{model.FrmYearPeriod}";
             var frmFolderKey = model.FrmDate.ToString("yyyy-MM-dd");
             model.FrmJobId = await _frmService.RunValidation(frmContainerName, frmFolderKey);
@@ -102,7 +106,7 @@
         [HttpPost]
         public async Task<IActionResult> PublishFrm(FrmReportModel model)
         {
-            model.FrmJobType = Constants.FrmPublishKey;
+            model.FrmJobType = Utils.Constants.FrmPublishKey;
             model.FrmJobId = await _frmService.RunPublish(model.FrmJobId);
 
             return RedirectToAction("HoldingPageAsync", model);
@@ -124,7 +128,7 @@
             {
                 var currentPeriod = await _periodService.ReturnPeriod();
 
-                var containerName = string.Format(Constants.FrmContainerName, currentPeriod.Year);
+                var containerName = string.Format(Utils.Constants.FrmContainerName, currentPeriod.Year);
 
                 var blobStream = await _storageService.GetFile(containerName, fileName, CancellationToken.None);
 
