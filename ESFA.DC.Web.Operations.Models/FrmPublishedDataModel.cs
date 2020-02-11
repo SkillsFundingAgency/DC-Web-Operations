7c1d3d3fe3967ea0908957fcc2fa4ce930946f28
David Feetenby <david.feetenby@eas.co.uk>
2020-02-11 12:08:22 +0000

    Added FrmPublishedDataModel

---
+++ ESFA.DC.Web.Operations.Models/FrmPublishedDataModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESFA.DC.Web.Operations.Areas.FRM.Models
+{
+    public class FrmPublishedDataModel
+    {
+        public int CalendarYear { get; set; }
+
+        public int PeriodNumber { get; set; }
+    }
+}
