85b82216b6963ce90530cef69dc659c270bcae6c
Yener Yilmaz <yener.yilmaz@nimbusdti.co.uk>
2020-02-13 13:55:28 +0000

    #92505 Add Download CSC button, add methed to contollerjs to prepare and download CSV

--- ESFA.DC.Web.Operations/wwwroot/assets/js/processing/jobProcessingController.js
+++ ESFA.DC.Web.Operations/wwwroot/assets/js/processing/jobProcessingController.js
@@ -1,4 +1,5 @@
 ﻿import { getColorForPercentage } from '/assets/js/util.js';
+import { convertToCsv } from '/assets/js/csv-operations.js';
 
 class JobProcessingController {
     constructor() {
@@ -39,11 +40,11 @@ class JobProcessingController {
         for (var i = 0; i < filteredData.length; i++) {
             var item = filteredData[i];
             sb.push(`<tr class="govuk-table__row">`);
-            sb.push(`<td class="govuk-table__cell" style="width:200px"><a href="#">${item.providerName}</a></td>`);
+            sb.push(`<td class="govuk-table__cell" style="width:250px"><a href="#">${item.providerName}</a></td>`);
             sb.push(`<td class="govuk-table__cell" style="width:100px">${item.ukprn}</td>`);
             sb.push(`<td class="govuk-table__cell" style="width:170px">${item.timeTaken}</td>`);
             sb.push(`<td class="govuk-table__cell" style="width:170px">${item.averageProcessingTime}</td>`);
-            sb.push(`<td class="govuk-table__cell" style="width:130px">${item.statusDescription}</td>`);
+            sb.push(`<td class="govuk-table__cell">${item.statusDescription}</td>`);
             sb.push(`</tr>`);
         }
         var result = sb.join('');
@@ -104,6 +105,19 @@ class JobProcessingController {
                     break;
             }
         }
+    }
+
+    downloadCSV(data) {
+        let newData = data.jobs.map(function (obj) {
+            return {
+                "Provider name": obj.providerName,
+                Ukprn: obj.ukprn,
+                "Time taken": obj.timeTaken,
+                "Average processing time": obj.averageProcessingTime,
+                "Job status": obj.statusDescription
+            }
+        });
+        convertToCsv({ filename: 'Jobs-processing.csv', data: newData });
     }
 }
 
