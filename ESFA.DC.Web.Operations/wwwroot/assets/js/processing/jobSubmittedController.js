4fb699d84053549479b5e5c9e3ffa45993ec33fb
Yener Yilmaz <yener.yilmaz@nimbusdti.co.uk>
2020-02-20 14:45:12 +0000

    #77401-92561 Add new lines and change CSV header.

--- ESFA.DC.Web.Operations/wwwroot/assets/js/processing/jobSubmittedController.js
+++ ESFA.DC.Web.Operations/wwwroot/assets/js/processing/jobSubmittedController.js
@@ -5,7 +5,9 @@ import { getFormattedDatetimeString } from '/assets/js/util.js';
 import { getDatetimeFromString } from '/assets/js/util.js';
 
 class JobSubmittedController {
+
     constructor() {
+
         this._firstDonut = document.getElementById("firstDonut");
         this._firstCircle = document.getElementById("firstCircle");
         this._firstDonutText = document.getElementById("firstDonutText");
@@ -20,9 +22,11 @@ class JobSubmittedController {
         this._percentageTextRangeJobSubmitted = [{ value: 85, label: 'Urgent Attention!' }, { value: 60, label: 'Needs Attention' }, { value: 0, label: 'Looking Good' }];
 
         this._data = {};
+
     }
 
-    updatePage(data) {        
+    updatePage(data) {
+
         if (typeof data === 'object') {
             this._data = data;
         }
@@ -30,13 +34,16 @@ class JobSubmittedController {
             this._data = JSON.parse(data);
             this._data.jobs.map(p => { p.datetime = getDatetimeFromString(p.createdDate), p.createdDateStr = getFormattedDatetimeString(p.createdDate) });
         }
+
         this.drawGrid();
+
     }
 
     setDonut(filteredData) {
+
         this._firstDonut.setAttribute("data-count", filteredData.length);
         this._firstDonut.textContent = filteredData.length;
-        
+
         let percentage = 100;
         if (filteredData.length < 2500) {
             percentage = (filteredData.length / 2500) * 100;
@@ -45,20 +52,25 @@ class JobSubmittedController {
         this._firstCircle.setAttribute("stroke-dasharray", `${percentage}, 100`);
         this._firstCircle.setAttribute("style", "stroke:" + getColorForPercentage(percentage));
         this._firstDonutText.textContent = getMessageForPercentage(percentage, this._percentageTextRangeJobSubmitted);
+        
     }
 
     displayConnectionState(state) {
+
         const stateLabel = document.getElementById("state");
         stateLabel.textContent = `Status: ${state}`;
+
     }
 
     drawGrid() {
+
         var filteredData = this.filterBy();
         this._aBtnDownloadCSV.style.visibility = (filteredData.length > 0) ? 'visible' : 'hidden';
         this.setDonut(filteredData);
         this.sortBy(filteredData);
 
         var sb = [];
+
         for (var i = 0; i < filteredData.length; i++) {
             var item = filteredData[i];
             sb.push(`<tr class="govuk-table__row">`);
@@ -69,20 +81,26 @@ class JobSubmittedController {
             sb.push(`<td class="govuk-table__cell">${item.statusDescription}</td>`);
             sb.push(`</tr>`);
         }
+
         var result = sb.join('');
 
         var dataContent = document.getElementById("dataContent");
         dataContent.innerHTML = result;
+
     }
 
     filterBy() {
+
         var filters = [];
+
         if (this._ilr && this._ilr.checked) {
             filters.push(this._ilr.value);
         }
+
         if (this._eas && this._eas.checked) {
             filters.push(this._eas.value);
         }
+
         if (this._esf && this._esf.checked) {
             filters.push(this._esf.value);
         }
@@ -94,19 +112,22 @@ class JobSubmittedController {
                     return anotherOne_el == array_el.collectionType;
                 }).length > 0
             });
+
         }
 
         return this._data.jobs;
+
     }
 
     sortBy(filteredData) {
+
         if (this._sort) {
             switch (this._sort.value) {
                 case 'Datetime':
                     filteredData.sort(function (a, b) {
                         return Number(a.datetime) - Number(b.datetime);
                     });
-                    break;                
+                    break;
                 case 'Alphabetical':
                     filteredData.sort(function (a, b) {
                         var nameA = a.providerName.toUpperCase();
@@ -127,22 +148,28 @@ class JobSubmittedController {
                     break;
             }
         }
+
     }
 
     downloadCSV() {
+
         var filteredData = this.filterBy();
+
         if (filteredData.length > 0) {
             let newData = filteredData.map(function (obj) {
                 return {
                     "Provider name": obj.providerName,
-                    Ukprn: obj.ukprn,
+                    "Ukprn": obj.ukprn,
                     "Date/time": obj.createdDateStr,
                     "Filename": obj.fileName,
                     "Job status": obj.statusDescription
                 }
             });
+
             convertToCsv({ filename: 'Jobs-Submitted.csv', data: newData });
+
         }
+
     }
 }
 
