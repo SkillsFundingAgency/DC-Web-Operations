e31ffa3e82d673e920e98e25495074986a0f5463
Yener Yilmaz <yener.yilmaz@nimbusdti.co.uk>
2020-03-23 12:52:24 +0000

    #97168 change javascript stringbuild array

--- ESFA.DC.Web.Operations/wwwroot/assets/js/periodEnd/validityPeriodController.js
+++ ESFA.DC.Web.Operations/wwwroot/assets/js/periodEnd/validityPeriodController.js
@@ -32,26 +32,26 @@
             var enabled = item.enabled ? true : false;
             var checked = enabled ? 'checked="checked"' : '';
 
-            sb.push(`<tr class="govuk-table__row">`);
-            sb.push(`<td class="govuk-table__cell">${item.collection}</td>`);
-            sb.push(`<td class="govuk-table__cell">
-                        <div class="govuk-checkboxes">
-                            <div class="flex">
-                                <div class="govuk-checkboxes__item">
-                                    <input class="govuk-checkboxes__input"
-                                           id="chkEnabled"
-                                           name="chkEnabled"
-                                           type="checkbox"
-                                           ${checked}
-                                           value="${enabled}" 
-                                           onchange="window.checkedChanged(${i}, this);"/>
-                                    <label class="govuk-label govuk-checkboxes__label">
-                                    </label>
+            sb.push(`<tr class="govuk-table__row">
+                         <td class="govuk-table__cell">${item.collection}</td>
+                         <td class="govuk-table__cell">
+                            <div class="govuk-checkboxes">
+                                <div class="flex">
+                                    <div class="govuk-checkboxes__item">
+                                        <input class="govuk-checkboxes__input"
+                                               id="chkEnabled"
+                                               name="chkEnabled"
+                                               type="checkbox"
+                                               ${checked}
+                                               value="${enabled}" 
+                                               onchange="window.checkedChanged(${i}, this);"/>
+                                        <label class="govuk-label govuk-checkboxes__label">
+                                        </label>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                     </td>`);
-            sb.push(`</tr>`);
+                       </td>
+                  </tr>`);
         }
         var result = sb.join('');
 
