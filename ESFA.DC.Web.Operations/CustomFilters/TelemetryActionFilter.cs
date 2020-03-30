f6be83ccfa62ceeecfdc41603cee1f951e04c13e
Pete Chamley <peter@chamley.name>
2020-03-30 18:45:33 +0100

    Put Application Insight Config back to Defaults. Add Customer Action Filter Add Customer Action Filter to startup..

---
+++ ESFA.DC.Web.Operations/CustomFilters/TelemetryActionFilter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ESFA.DC.Web.Operations.CustomFilters
+{
+    public class TelemetryActionFilter : IAsyncActionFilter
+    {
+        private readonly TelemetryClient _telemetryClient;
+
+        public TelemetryActionFilter(TelemetryClient telemetryClient)
+        {
+            _telemetryClient = telemetryClient;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            SendTelemetry(context);
+            var resultContext = await next();
+        }
+
+        private void SendTelemetry(ActionExecutingContext context)
+        {
+            var telemetryArgs = new Dictionary<string, string>();
+            foreach (var arg in context.ActionArguments)
+            {
+                telemetryArgs.Add(arg.Key, arg.Value.ToString());
+            }
+
+            _telemetryClient.TrackTrace(context.ActionDescriptor.DisplayName, telemetryArgs);
+        }
+    }
+}
