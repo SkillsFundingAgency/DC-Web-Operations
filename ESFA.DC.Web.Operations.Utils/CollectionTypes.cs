eee257ad78e22357ed1789e1d6fd0e6f7e07a3c0
Greg Hunter <GregHunterEsfa@outlook.com>
2020-04-27 11:26:34 +0100

    Changed summarysation history to look for specified period not latest.

--- ESFA.DC.Web.Operations.Utils/CollectionTypes.cs
+++ ESFA.DC.Web.Operations.Utils/CollectionTypes.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ESFA.DC.Web.Operations.Utils
+﻿namespace ESFA.DC.Web.Operations.Utils
 {
     public static class CollectionTypes
     {
         public const string ILR = "ILR";
         public const string NCS = "NCS";
         public const string ALLF = "Generic";
+        public const string ESF = "ESF";
+        public const string APPS = "APPS";
     }
 }
