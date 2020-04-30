4d2b59654e3aeb6dc809d64551863282352134c6
Hari Dupati <dupatihari@gmail.com>
2020-04-30 12:23:09 +0100

    97392 - NCS Reports page

---
+++ ESFA.DC.Web.Operations/Areas/PeriodEndNCS/Controllers/PeriodEndReportController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ESFA.DC.Logging.Interfaces;
+using ESFA.DC.Web.Operations.Areas.PeriodEndNCS.Models;
+using ESFA.DC.Web.Operations.Controllers;
+using ESFA.DC.Web.Operations.Interfaces.PeriodEnd;
+using ESFA.DC.Web.Operations.Interfaces.Storage;
+using ESFA.DC.Web.Operations.Models.Summarisation;
+using ESFA.DC.Web.Operations.Utils;
+using Microsoft.ApplicationInsights;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ESFA.DC.Web.Operations.Areas.PeriodEndNCS.Controllers
+{
+    [Area(AreaNames.PeriodEndNCS)]
+    [Route(AreaNames.PeriodEndNCS + "/periodEndReports")]
+    public class PeriodEndReportController : BaseControllerWithOpsPolicy
+    {
+        private readonly IPeriodService _periodService;
+        private readonly INCSPeriodEndService _periodEndService;
+        private readonly IStorageService _storageService;
+        private readonly ILogger _logger;
+
+        public PeriodEndReportController(
+            IPeriodService periodService,
+            INCSPeriodEndService periodEndService,
+            IStorageService storageService,
+            ILogger logger,
+            TelemetryClient telemetryClient)
+            : base(logger, telemetryClient)
+        {
+            _logger = logger;
+            _periodService = periodService;
+            _periodEndService = periodEndService;
+            _storageService = storageService;
+        }
+
+        [HttpGet("{collectionYear?}/{period?}")]
+        public async Task<IActionResult> Index(int? collectionYear, int? period, CancellationToken cancellationToken)
+        {
+            var currentYearPeriod = await _periodService.ReturnPeriod(CollectionTypes.NCS, cancellationToken);
+            if (currentYearPeriod.Year == null)
+            {
+                throw new Exception($"Return period {currentYearPeriod.Period} has no year.");
+            }
+
+            var model = new PeriodEndReportViewModel();
+
+            if (collectionYear != null && period != null)
+            {
+                model.Year = collectionYear.Value;
+                model.Period = period.Value;
+            }
+            else
+            {
+                model.Year = currentYearPeriod.Year.Value;
+                model.Period = currentYearPeriod.Period;
+            }
+
+            model.ReportDetails = await _periodEndService.GetPeriodEndReportsAsync(model.Year, model.Period, cancellationToken);
+
+            return View(model);
+        }
+
+        [Route("getReportFile/{collectionYear}/{*fileName}")]
+        public async Task<FileResult> GetReportFile(int collectionYear, string fileName, string downloadName = "")
+        {
+            try
+            {
+                var containerName = Utils.Constants.PeriodEndBlobContainerName.Replace(Utils.Constants.CollectionYearToken, collectionYear.ToString());
+                var blobStream = await _storageService.GetFile(containerName, fileName, CancellationToken.None);
+
+                return new FileStreamResult(blobStream, _storageService.GetMimeTypeFromFileName(fileName))
+                {
+                    FileDownloadName = string.IsNullOrEmpty(downloadName) ? fileName : downloadName
+                };
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Download report failed for file name : {fileName}", e);
+                throw;
+            }
+        }
+
+        [Route("getSampleReport/{collectionYear}/{period}/{*fileName}")]
+        public async Task<FileResult> GetReportFile(int collectionYear, int period, string fileName)
+        {
+            var downloadName = $"{fileName.Substring(fileName.IndexOf('/') + 1, 8)}_{collectionYear}_R{period.ToString().PadLeft(2, '0')}_Reports.zip";
+
+            return await GetReportFile(collectionYear, fileName, downloadName);
+        }
+    }
+}
