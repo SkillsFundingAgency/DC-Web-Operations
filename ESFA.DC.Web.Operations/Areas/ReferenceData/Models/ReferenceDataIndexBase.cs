1f2d582d96e138a425255a9e85bf6200bd05a984
David Gill <david.gill@nimbusdti.co.uk>
2020-06-08 09:12:19 +0100

    #101012 work for campus identifiers

---
+++ ESFA.DC.Web.Operations/Areas/ReferenceData/Models/ReferenceDataIndexBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESFA.DC.Web.Operations.Areas.ReferenceData.Models
+{
+    public class ReferenceDataIndexBase
+    {
+        public DateTime LastUpdatedDateTime { get; set; }
+
+        public string LastUpdatedByWho { get; set; }
+    }
+}
---
+++ ESFA.DC.Web.Operations/Areas/ReferenceData/Models/ReferenceDataIndexModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESFA.DC.Web.Operations.Areas.ReferenceData.Models
+{
+    public class ReferenceDataIndexModel
+    {
+        public ReferenceDataIndexBase CampusIdentifiers { get; set; }
+    }
+}
