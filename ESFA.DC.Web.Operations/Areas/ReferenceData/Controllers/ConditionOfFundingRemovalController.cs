ce2e749ad96bf529bc234493db6277da797caa70
jamie <jamie.coales@education.gov.uk>
2020-06-08 13:46:44 +0100

    stub

---
+++ ESFA.DC.Web.Operations/Areas/ReferenceData/Controllers/ConditionOfFundingRemovalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ESFA.DC.Web.Operations.Areas.ReferenceData.Controllers
+{
+    public class ConditionOfFundingRemovalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
