c3ffdbe64a54baa533eed4ed2e97e72bd7c50fe4
Hari Dupati <dupatihari@gmail.com>
2020-07-14 12:29:44 +0100

    wip

---
+++ ESFA.DC.Web.Operations/wwwroot/assets/js/handlebars-helpers.js
@@ -0,0 +1,40 @@
+function getHandleBarsTemplate(name, root) {
+    if (root === undefined) {
+        root = '/assets/templates/';
+    }
+    if (Handlebars.templates === undefined || Handlebars.templates[name] === undefined) {
+        var xmlhttp;
+        if (window.XMLHttpRequest) {
+            xmlhttp = new XMLHttpRequest();
+        }
+        else {
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState === XMLHttpRequest.DONE) {
+                if (xmlhttp.status === 200) {
+                    if (Handlebars.templates === undefined) {
+                        Handlebars.templates = {};
+                    }
+                    Handlebars.templates[name] = Handlebars.compile(xmlhttp.responseText);
+                    return Handlebars.templates[name];
+                }
+                else {
+                    console.log('Error fetching the template.');
+                }
+            }
+        }
+        xmlhttp.open("GET", root + name + '.html', true);
+        xmlhttp.send();
+
+    }
+    return Handlebars.templates[name];
+};
+
+Handlebars.registerHelper('jobStatusClass', function (displayStatus) {
+    var statusClass = displayStatus === 'Job Completed' ? 'jobCompleted'
+                    : displayStatus === 'Job Rejected' ? 'jobRejected'
+                    : displayStatus === 'Job Failed' ? 'jobFailed'
+                    : '';
+    return statusClass;
+});
--- ESFA.DC.Web.Operations/wwwroot/assets/js/referenceData/referenceDataController.js
+++ ESFA.DC.Web.Operations/wwwroot/assets/js/referenceData/referenceDataController.js
@@ -32,35 +32,38 @@ class referenceDataController {
 
         this.sortByDate(stateModel);
 
-        const fileContainer = document.getElementById('fileContainer');
+        var compiledTemplate = getHandleBarsTemplate('RefDataListing');
+        document.getElementById("filesList").innerHTML = compiledTemplate({ viewModel: stateModel, controllerName: controllerName });
 
-        let updatedContent = '';
+        //const fileContainer = document.getElementById('fileContainer');
 
-        stateModel.files.forEach(function(file) {
-            const reportName = file.reportName ? file.reportName : '';
-            const statusClass = file.displayStatus === 'Job Completed' ? 'jobCompleted' 
-                : file.displayStatus === 'Job Rejected' ? 'jobRejected'
-                : file.displayStatus === 'Job Failed' ? 'jobFailed'
-                : '';
+        //let updatedContent = '';
 
-            const content = 
-                `<tr class="govuk-table__row">
-                    <td class="govuk-table__cell govuk-!-font-weight-bold" govuk-!-width-one-quarter>${file.displayDate}</td>
-                    <td class="govuk-table__cell">${file.submittedBy}</td>
-                    <td class="govuk-table__cell"><a href="/referenceData/${controllerName}/getReportFile/${file.fileName}">${file.fileName}</a></td>
-                    <td class="govuk-table__cell">${file.jobId}</td>
-                    <td class="govuk-table__cell">
-                        <span class="${statusClass}">${file.displayStatus}</span> <br />
-                        <span class="govuk-!-font-weight-bold">${file.recordCount} records</span> <br />
-                        <span class="govuk-!-font-weight-bold">${file.warningCount} warnings</span> <br />
-                        <span class="govuk-!-font-weight-bold">${file.errorCount} errors</span> <br />
-                    </td>
-                    <td class="govuk-table__cell"><a href="/referenceData/${controllerName}/getReportFile/${file.reportName}/${file.jobId}">${reportName}</a></td>
-                </tr>`;
-            updatedContent += content;
-        });
+        //stateModel.files.forEach(function(file) {
+        //    const reportName = file.reportName ? file.reportName : '';
+        //    const statusClass = file.displayStatus === 'Job Completed' ? 'jobCompleted' 
+        //        : file.displayStatus === 'Job Rejected' ? 'jobRejected'
+        //        : file.displayStatus === 'Job Failed' ? 'jobFailed'
+        //        : '';
 
-        fileContainer.innerHTML = updatedContent;
+        //    const content = 
+        //        `<tr class="govuk-table__row">
+        //            <td class="govuk-table__cell govuk-!-font-weight-bold" govuk-!-width-one-quarter>${file.displayDate}</td>
+        //            <td class="govuk-table__cell">${file.submittedBy}</td>
+        //            <td class="govuk-table__cell"><a href="/referenceData/${controllerName}/getReportFile/${file.fileName}">${file.fileName}</a></td>
+        //            <td class="govuk-table__cell">${file.jobId}</td>
+        //            <td class="govuk-table__cell">
+        //                <span class="${statusClass}">${file.displayStatus}</span> <br />
+        //                <span class="govuk-!-font-weight-bold">${file.recordCount} records</span> <br />
+        //                <span class="govuk-!-font-weight-bold">${file.warningCount} warnings</span> <br />
+        //                <span class="govuk-!-font-weight-bold">${file.errorCount} errors</span> <br />
+        //            </td>
+        //            <td class="govuk-table__cell"><a href="/referenceData/${controllerName}/getReportFile/${file.reportName}/${file.jobId}">${reportName}</a></td>
+        //        </tr>`;
+        //    updatedContent += content;
+        //});
+
+        //fileContainer.innerHTML = updatedContent;
     }
 }
 
