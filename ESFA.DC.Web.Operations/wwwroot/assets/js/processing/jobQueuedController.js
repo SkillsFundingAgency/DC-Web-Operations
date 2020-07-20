f2082468b49d4def143636645bf7d4af125379fb
Martin Freshwater <martyfreshwater@hotmail.com>
2020-07-20 09:59:54 +0000

    102693:  Job queued hub refactor.

--- ESFA.DC.Web.Operations/wwwroot/assets/js/processing/jobQueuedController.js
+++ ESFA.DC.Web.Operations/wwwroot/assets/js/processing/jobQueuedController.js
@@ -37,6 +37,10 @@ class JobQueuedController {
 
     }
 
+    registerHandlers(hub) {
+        hub.registerMessageHandler("ReceiveMessage", (data) => this.updatePage(data));
+    }
+
     setDonut(filteredData) {
         this._firstDonut.setAttribute("data-count", filteredData.length);
         this._firstDonut.textContent = filteredData.length;
