2971a6501d346892aebae31218265de55ab1cce0
David Feetenby <david.feetenby@eas.co.uk>
2020-07-22 10:54:26 +0100

    changed varchar values from max to more standard size. Left old and new values as max

--- AuditDatabase/dbo/Tables/AuditEventType.sql
+++ AuditDatabase/dbo/Tables/AuditEventType.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[AuditEventType]
 (	[ID] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
     [EventId] [int] NOT NULL,
-    [EventTitle] [nvarchar](max) NOT NULL,
-    [EventDescription] [nvarchar](max) NOT NULL, 
-    [EventOperationType] NVARCHAR(MAX) NOT NULL
+    [EventTitle] VARCHAR(50) NOT NULL,
+    [EventDescription] [nvarchar](500) NOT NULL, 
+    [EventOperationType] NVARCHAR(50) NOT NULL
 ) 
 GO
