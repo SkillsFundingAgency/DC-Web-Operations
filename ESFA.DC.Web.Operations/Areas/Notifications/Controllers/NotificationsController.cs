fecb1e143ff499c25adb2bb1e3976291123088f9
Salman Baig <salman.sfa@outlook.com>
2020-07-28 15:05:42 +0100

    notifications

--- ESFA.DC.Web.Operations/Areas/Notifications/Controllers/NotificationsController.cs
+++ ESFA.DC.Web.Operations/Areas/Notifications/Controllers/NotificationsController.cs
@@ -1,7 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using ESFA.DC.Logging.Interfaces;
+using ESFA.DC.Web.Operations.Constants;
 using ESFA.DC.Web.Operations.Controllers;
 using ESFA.DC.Web.Operations.Interfaces.Notifications;
 using ESFA.DC.Web.Operations.Models.Notifications;
@@ -39,6 +41,11 @@ namespace ESFA.DC.Web.Operations.Areas.Notifications.Controllers
 
         public async Task<IActionResult> Save(Notification model, CancellationToken cancellationToken)
         {
+            if (!Validate(model))
+            {
+                return View("Manage", model);
+            }
+
             var result = await _notificationsService.SaveNotificationAsync(cancellationToken, model);
 
             if (result)
@@ -47,6 +54,54 @@ namespace ESFA.DC.Web.Operations.Areas.Notifications.Controllers
             }
 
             return View("Manage", model);
+        }
+
+        private bool Validate(Notification model)
+        {
+            if (string.IsNullOrEmpty(model?.Message))
+            {
+                ModelState.AddModelError(ErrorMessageKeys.Submission_FileFieldKey, "Please enter message");
+                ModelState.AddModelError(ErrorMessageKeys.ErrorSummaryKey, "Please enter message");
+
+                return false;
+            }
+
+            if (model.StartDate == DateTime.MinValue || model.StartTime == DateTime.MinValue)
+            {
+                ModelState.AddModelError(ErrorMessageKeys.Submission_FileFieldKey, "Please enter valid start date and time");
+                ModelState.AddModelError(ErrorMessageKeys.ErrorSummaryKey, "Please enter valid start date and time");
+
+                return false;
+            }
+
+            if (model.EndDate == DateTime.MinValue || model.EndTime == DateTime.MinValue)
+            {
+                ModelState.AddModelError(ErrorMessageKeys.Submission_FileFieldKey, "Please enter valid end date and time");
+                ModelState.AddModelError(ErrorMessageKeys.ErrorSummaryKey, "Please enter valid end date and time");
+
+                return false;
+            }
+
+            if (model.StartTime == DateTime.MinValue || model.EndTime == DateTime.MinValue)
+            {
+                ModelState.AddModelError(ErrorMessageKeys.Submission_FileFieldKey, "Please enter valid end date and time");
+                ModelState.AddModelError(ErrorMessageKeys.ErrorSummaryKey, "Please enter valid end date and time");
+
+                return false;
+            }
+
+            var startDateTime = model.StartDate.Date.Add(model.StartTime.TimeOfDay);
+            var endDateTime = model.EndDate.Date.Add(model.EndTime.TimeOfDay);
+
+            if (startDateTime > endDateTime)
+            {
+                ModelState.AddModelError(ErrorMessageKeys.Submission_FileFieldKey, "Start date/time can not be after end date/time.");
+                ModelState.AddModelError(ErrorMessageKeys.ErrorSummaryKey, "Start date/time can not be after end date/time.");
+
+                return false;
+            }
+
+            return true;
         }
     }
 }
