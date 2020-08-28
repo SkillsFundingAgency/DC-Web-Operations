3a0e995e0d3a1fc7d7bcd1df5c0c3cdcf03e9de9
David Feetenby <david.feetenby@eas.co.uk>
2020-08-28 12:28:08 +0100

    removed unimplemented methods from ibasehttpclientservice

--- ESFA.DC.Web.Operations.Interfaces/IBaseHttpClientService.cs
+++ ESFA.DC.Web.Operations.Interfaces/IBaseHttpClientService.cs
@@ -8,22 +8,10 @@ namespace ESFA.DC.Web.Operations.Interfaces
 {
     public interface IBaseHttpClientService
     {
-        Task<string> SendDataAsync(string url, object data, CancellationToken cancellationToken, string username = null, DifferentiatorPath? differentiator = null);
-
         Task<HttpRawResponse> SendDataAsyncRawResponse(string url, object data, CancellationToken cancellationToken, string username = null, DifferentiatorPath? differentiator = null);
 
         Task<string> SendAsync(string url, CancellationToken cancellationToken);
 
-        Task<string> GetDataAsync(string url, CancellationToken cancellationToken);
-
         Task<T> GetAsync<T>(string url, CancellationToken cancellationToken);
-
-        Task<string> PutDataAsync(string url, object data, CancellationToken cancellationToken);
-
-        Task DeleteAsync(string url, CancellationToken cancellationToken);
-
-        Task<TResult> PostAsync<TContent, TResult>(string baseUrl, TContent content, IEnumerable<string> segments = null, CancellationToken cancellationToken = default);
-
-        Task<TResult> GetAsync<TResult>(string baseUrl, IEnumerable<string> segments = null, CancellationToken cancellationToken = default);
     }
 }
