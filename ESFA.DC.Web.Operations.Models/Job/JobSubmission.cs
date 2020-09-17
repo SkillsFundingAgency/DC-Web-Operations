e57fddec06fb5e5ac56a88afbc951b2127a7f7e7
David Gill <david.gill@nimbusdti.co.uk>
2020-09-17 13:39:52 +0100

    #104435 remove un-necessary code

--- ESFA.DC.Web.Operations.Models/Job/JobSubmission.cs
+++ ESFA.DC.Web.Operations.Models/Job/JobSubmission.cs
@@ -25,9 +25,5 @@ namespace ESFA.DC.Web.Operations.Models.Job
         public int CollectionYear { get; set; }
 
         public bool? TermsAccepted { get; set; }
-
-        public string DesktopReferenceDataStoragePath { get; set; }
-
-        public string IlrReferenceData { get; set; }
     }
 }
