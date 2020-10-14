d718a8647da09f6eaf7d1273196649b1fcf35c5a
David Gill <david.gill@nimbusdti.co.uk>
2020-10-14 15:50:11 +0100

    #105510 update following code review

--- ESFA.DC.Web.Operations.Services/FileValidation/FileNameValidationServiceProvider.cs
+++ ESFA.DC.Web.Operations.Services/FileValidation/FileNameValidationServiceProvider.cs
@@ -2,52 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using ESFA.DC.Web.Operations.Interfaces;
-using ESFA.DC.Web.Operations.Interfaces.Collections;
 using ESFA.DC.Web.Operations.Interfaces.ReferenceData;
-using ESFA.DC.Web.Operations.Interfaces.Storage;
 using ESFA.DC.Web.Operations.Services.FileValidation.StandardValidator;
-using ESFA.DC.Web.Operations.Settings.Models;
 
 namespace ESFA.DC.Web.Operations.Services.FileValidation
 {
     public class FileNameValidationServiceProvider : IFileNameValidationServiceProvider
     {
-        private readonly IStorageService _storageService;
-        private readonly FeatureFlags _featureFlags;
-        private readonly IJobService _jobService;
-        private readonly ICollectionsService _collectionService;
-        private readonly AzureStorageSection _azureStorageSection;
-        private readonly IEnumerable<ICollection> _collections;
         private readonly IEnumerable<IFileNameValidationService> _fileNameValidationServices;
 
         public FileNameValidationServiceProvider(
-            IStorageService storageService,
-            FeatureFlags featureFlags,
-            IJobService jobService,
-            ICollectionsService collectionService,
-            AzureStorageSection azureStorageSection,
-            IEnumerable<ICollection> collections,
             IEnumerable<IFileNameValidationService> fileNameValidationServices)
         {
-            _storageService = storageService;
-            _featureFlags = featureFlags;
-            _jobService = jobService;
-            _collectionService = collectionService;
-            _azureStorageSection = azureStorageSection;
-            _collections = collections;
             _fileNameValidationServices = fileNameValidationServices;
         }
 
         public IFileNameValidationService GetFileNameValidationService(string collectionName)
         {
             return _fileNameValidationServices.FirstOrDefault(x => x.CollectionName.Equals(collectionName, StringComparison.CurrentCultureIgnoreCase)) ??
-                   new StandardFileNameValidationService(
-                    _storageService,
-                    _featureFlags,
-                    _jobService,
-                    _collectionService,
-                    _azureStorageSection,
-                    _collections);
+                _fileNameValidationServices.FirstOrDefault(x => x.GetType() == typeof(StandardFileNameValidationService));
         }
     }
 }
