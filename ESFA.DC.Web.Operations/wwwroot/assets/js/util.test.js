3cf65c00a08b75a323ba8fd864904385cae7aa38
David Gill <david.gill@nimbusdti.co.uk>
2020-11-09 08:41:28 +0000

    #106292 added tests

--- ESFA.DC.Web.Operations/wwwroot/assets/js/util.test.js
+++ ESFA.DC.Web.Operations/wwwroot/assets/js/util.test.js
@@ -3,3 +3,21 @@
 test('removeSpaces removes all spaces', () => {
     expect(removeSpaces(" remove all    spaces ")).toBe("removeallspaces");
 });
+
+import { sToTime } from './util.js'; describe("sToTime", () => {
+    test('converts to correct hours', () => {
+        expect(sToTime(3600)).toBe('01: 00: 00 secs');
+    });
+});
+
+describe("sToTime", () => {
+    test('converts to correct mins', () => {
+        expect(sToTime(60)).toBe('00: 01: 00 secs');
+    });
+});
+
+describe("sToTime", () => {
+    test('converts to correct secs', () => {
+        expect(sToTime(30)).toBe('00: 00: 30 secs');
+    });
+});
