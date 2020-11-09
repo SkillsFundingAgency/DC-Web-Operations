d1f09cd3ad307ba322bf552aaaa862797ff8e0de
David Gill <david.gill@nimbusdti.co.uk>
2020-11-09 08:49:27 +0000

    #106292 minor refactor

--- ESFA.DC.Web.Operations/wwwroot/assets/js/util.test.js
+++ ESFA.DC.Web.Operations/wwwroot/assets/js/util.test.js
@@ -1,22 +1,22 @@
-﻿import { removeSpaces } from './util.js';
+﻿import { removeSpaces, sToTime } from './util.js';
 
-test('removeSpaces removes all spaces', () => {
-    expect(removeSpaces(" remove all    spaces ")).toBe("removeallspaces");
-});
+describe("removeSpaces",
+    () => {
+        test('removeSpaces removes all spaces',
+            () => {
+                expect(removeSpaces(" remove all    spaces ")).toBe("removeallspaces");
+            });
+    });
 
-import { sToTime } from './util.js'; describe("sToTime", () => {
+describe("sToTime", () => {
     test('converts to correct hours', () => {
         expect(sToTime(3600)).toBe('01: 00: 00 secs');
     });
-});
 
-describe("sToTime", () => {
     test('converts to correct mins', () => {
         expect(sToTime(60)).toBe('00: 01: 00 secs');
     });
-});
 
-describe("sToTime", () => {
     test('converts to correct secs', () => {
         expect(sToTime(30)).toBe('00: 00: 30 secs');
     });
